762a98a2eb45447398850c321a7226e29db5555c
Matthew Lin <Matthew Lin>
2017-04-22 18:09:37 -0400

    Cleaned up redundant components

--- app/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var Message = ({temp, location}) => {
-  return (
-    <h3 className="text-center">It's {temp} degrees in {location}.</h3>
-  )
-};
-
-module.exports = Message;
